30d8d5e7991a8056391a719490a7ac4e84800f1e
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-02-06 16:27:33 +0100

    Add gridworld and test cases

---
+++ gridworld/gridworld.py
@@ -0,0 +1,153 @@
+from pathlib import Path
+from enum import IntEnum
+from typing import Union, Iterable
+from tkinter import Canvas, Tk
+
+import gym
+import numpy as np
+
+
+class CellType(IntEnum):
+    FREE = 0
+    AGENT = 1
+    WALL = 2
+    REWARD = 3
+
+
+class Gridworld(gym.Env):
+
+    # up, right, down, left
+    move_offset = ((-1, 0), (0, 1), (1, 0), (0, -1))
+    colors = {
+        CellType.AGENT: '#00FF00',
+        CellType.REWARD: '#FF0000',
+        CellType.FREE: '#FFFFFF',
+        CellType.WALL: '#505050'
+    }
+
+    def __init__(self, grid_h: int,
+                 grid_w: int,
+                 time_limit: int,
+                 reward: float,
+                 step_reward: float,
+                 discount: float):
+        super(Gridworld, self).__init__()
+        self._grid = np.zeros((grid_h, grid_w), dtype=np.int8)
+        self._init_grid = np.zeros((grid_h, grid_w), dtype=np.int8)
+        self._grid.flags.writeable = False
+        self.grid_h = grid_h
+        self.grid_w = grid_w
+        self.time_limit = time_limit
+        self.reward = reward
+        self.step_reward = step_reward
+        self.discount = discount
+
+        self.action_space = gym.spaces.Discrete(n=4)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(2,), dtype=np.int8)
+
+        self.canvas = None
+        self._tk_master = None
+        self._canvas_w = None
+        self._canvas_h = None
+        self._cell_w = None
+        self._cell_h = None
+
+    @property
+    def grid(self):
+        return self._grid
+
+    @grid.setter
+    def grid(self, new_grid):
+        self._grid.flags.writeable = True
+        self._grid[:] = new_grid[:]
+        self._grid.flags.writeable = False
+        self._init_grid[:] = new_grid[:]
+
+    def add_object(self, pos: Iterable[int], object_type: CellType):
+        self._grid.flags.writeable = True
+        self._grid[tuple(pos)] = object_type
+        self._grid.flags.writeable = False
+        self._init_grid[:] = self._grid
+
+    @staticmethod
+    def from_cleartext(path: Union[str, Path]):
+        args = {}
+        map_data_section = -1
+        grid = None
+        lookup_table = {'.': CellType.FREE, 'a': CellType.AGENT, 'r': CellType.REWARD, 'x': CellType.WALL}
+
+        with open(Path(path), 'r') as map_data_file:
+            for i_l, line in enumerate(map_data_file):
+                line_clean = ''.join(line.split())  # removes tabs, newlines etc.
+                if grid is None:
+                    k, v = line_clean.split(':')
+                    if k == 'map_data':
+                        map_data_section = i_l + 1
+                        grid = np.zeros((args['grid_h'], args['grid_w']), dtype=np.int8)
+                    elif k == 'grid_h' or k == 'grid_w' or k == 'time_limit':
+                        args[k] = int(v)
+                    else:
+                        args[k] = float(v)
+                else:
+                    for i_c, column in enumerate(line_clean):
+                        grid[i_l - map_data_section][i_c] = lookup_table[column.lower()]
+            gridworld = Gridworld(**args)
+            gridworld.grid = grid
+            return gridworld
+
+    def step(self, action: int):
+        if not self.action_space.contains(action):
+            raise ValueError(f'Action {action} is not a member of this environment\'s action space')
+
+        self._grid.flags.writeable = True
+
+        pos_agent = self._get_agent_cell()
+        dest_pos = pos_agent + self.move_offset[action]
+        dest_pos_clipped = np.clip(dest_pos, (0, 0), (self.grid_h - 1, self.grid_w - 1))
+        dest_type = self._grid[tuple(dest_pos_clipped)]
+        #print(f'dest_pos: {dest_pos}')
+        #print(f'dest_type: {dest_type}')
+
+        reward = 0
+        done = False
+        info = {}
+
+        if dest_type == CellType.FREE:
+            self._grid[tuple(pos_agent)] = CellType.FREE
+            self._grid[tuple(dest_pos_clipped)] = CellType.AGENT
+        elif dest_type == CellType.REWARD:
+            reward = self.reward
+            done = True
+
+        self._grid.flags.writeable = False
+
+        return self._grid, reward, done, info
+
+    def reset(self):
+        self._grid.flags.writeable = True
+        self._grid[:] = self._init_grid[:]
+        self._grid.flags.writeable = False
+        return self._grid
+
+    def render(self, mode="human"):
+        if self.canvas is None:
+            self._tk_master = Tk()
+            self._canvas_h, self._canvas_w = 300, 300
+            self._cell_h, self._cell_w = round(self._canvas_h / self.grid_h), round(self._canvas_w / self.grid_w)
+            self.canvas = Canvas(self._tk_master, width=self._canvas_w, height=self._canvas_h)
+            self.canvas.pack()
+
+        for y in range(self.grid_h):
+            for x in range(self.grid_w):
+                x0, y0 = self._cell_w * x, self._cell_h * y
+                x1, y1 = x0 + self._cell_w, y0 + self._cell_h
+                color = self.colors[self._grid[y, x]]
+                self.canvas.create_rectangle(x0, y0, x1, y1, fill=color)
+                self._tk_master.update()
+
+
+    def _get_agent_cell(self):
+        agent_pos = np.argwhere(self._grid == CellType.AGENT).squeeze()
+        if agent_pos.shape != (2,):
+            raise RuntimeError('No agent found in gridworld, please add agent to grid before calling step()')
+        return agent_pos
---
+++ tests/test_gridworld.py
@@ -0,0 +1,126 @@
+from pathlib import Path
+import unittest
+from time import sleep
+
+import numpy as np
+
+from gridworld.gridworld import Gridworld, CellType
+
+
+class GridworldTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.world = Gridworld(5, 5, 100, 1.0, 0.0, 0.99)
+        self.agent_start_pos = (2, 2)
+        self.reward_pos = (0, 4)
+        self.wall_pos = (3, 1)
+
+        self.world.add_object(self.agent_start_pos, CellType.AGENT)
+        self.world.add_object(self.reward_pos, CellType.REWARD)
+        self.world.add_object(self.wall_pos, CellType.WALL)
+
+    def test_from_cleartext(self):
+        world = Gridworld.from_cleartext(Path(__file__).parent / 'testmap.mapdata')
+
+        self.assertEqual(world.grid_h, 8)
+        self.assertEqual(world.grid_w, 8)
+        self.assertEqual(world.grid[0, 0], CellType.WALL.value)
+        self.assertEqual(world.grid[-1, -1], CellType.WALL.value)
+        self.assertEqual(world.grid[3, 1], CellType.WALL.value)
+        self.assertEqual(world.grid[0, -1], CellType.REWARD.value)
+        self.assertEqual(world.grid[-2, 1], CellType.AGENT.value)
+        self.assertEqual(world.grid[2, 2], CellType.FREE.value)
+
+    def test_reset(self):
+        world = self.world
+
+        # move away from start position
+        for action in [0, 0, 1]:
+            world.step(action)
+        self.assertEqual(world.grid[self.agent_start_pos], CellType.FREE)
+        self.assertEqual(world.grid[self.reward_pos], CellType.REWARD)
+        self.assertEqual(world.grid[self.wall_pos], CellType.WALL)
+
+        s = world.reset()
+        self.assertTrue((s == world.grid).all())
+        self.assertTrue((s == world._init_grid).all())
+        self.assertEqual(world.grid[self.agent_start_pos], CellType.AGENT)
+        self.assertEqual(world.grid[self.reward_pos], CellType.REWARD)
+        self.assertEqual(world.grid[self.wall_pos], CellType.WALL)
+
+    def test_step_basic_moving(self):
+        world = self.world
+
+        for action in [0, 1, 2, 3]:
+            agent_pos = np.array(self.agent_start_pos) + world.move_offset[action]
+            world.step(action)
+            self.assertEqual(world.grid[tuple(agent_pos)], CellType.AGENT)
+            world.reset()
+
+    def test_step_map_boundary_collision(self):
+        world = self.world
+
+        for action in [0, 1, 2, 3]:
+            agent_pos = np.array(self.agent_start_pos)
+            for _ in range(2):
+                world.step(action)
+                agent_pos += world.move_offset[action]
+            world.step(action)  # don't update agent_pos after this one since we're bumping into the map border
+            self.assertEqual(world.grid[tuple(agent_pos)], CellType.AGENT)
+            world.reset()
+
+    def test_step_wall_collision(self):
+        world = self.world
+
+        world.step(3)
+        self.assertEqual(world.grid[2, 1], CellType.AGENT)
+        world.step(2)
+        self.assertEqual(world.grid[2, 1], CellType.AGENT)
+        world.step(3)
+        self.assertEqual(world.grid[2, 0], CellType.AGENT)
+        world.step(2)
+        self.assertEqual(world.grid[3, 0], CellType.AGENT)
+        world.step(1)
+        self.assertEqual(world.grid[3, 0], CellType.AGENT)
+        world.step(2)
+        self.assertEqual(world.grid[4, 0], CellType.AGENT)
+        world.step(1)
+        self.assertEqual(world.grid[4, 1], CellType.AGENT)
+        world.step(0)
+        self.assertEqual(world.grid[4, 1], CellType.AGENT)
+        world.step(1)
+        self.assertEqual(world.grid[4, 2], CellType.AGENT)
+        world.step(0)
+        self.assertEqual(world.grid[3, 2], CellType.AGENT)
+        world.step(3)
+        self.assertEqual(world.grid[3, 2], CellType.AGENT)
+        world.step(0)
+        self.assertEqual(world.grid[self.agent_start_pos], CellType.AGENT)
+
+    def test_step_return_values(self):
+        world = self.world
+
+        actions = [0, 0, 0, 1, 1]
+        rewards = [0, 0, 0, 0, 1.0]
+        dones = [False, False, False, False, True]
+
+        for a, r_target, done_target in zip(actions, rewards, dones):
+            s, r, done, info = world.step(a)
+            self.assertTrue((s == world.grid).all())
+            self.assertEqual(r, r_target)
+            self.assertEqual(done, done_target)
+
+    @unittest.skip
+    def test_render(self):
+        world = self.world
+
+        world.reset()
+        for t in range(10):
+            world.render()
+            o, r, done, info = world.step(world.action_space.sample())
+            if done:
+                break
+            sleep(1)
+
+if __name__ == '__main__':
+    unittest.main()
