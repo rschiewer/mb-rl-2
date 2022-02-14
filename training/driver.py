e92854f452ca94751b9c7f5f33d35d83c62c2e20
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-02-14 14:24:44 +0100

    Update interface and description

--- training/driver.py
+++ training/driver.py
@@ -1,13 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Dict, List
+
+import numpy as np
 
 
 class Driver(ABC):
 
     @abstractmethod
-    def interact(self, *args, **kwargs) -> Any:
+    def interact(self,
+                 *args,
+                 **kwargs) -> List[Dict[str: np.ndarray, str: np.ndarray, str: np.ndarray, str: np.ndarray]]:
         """
         Collect some experience by interacting with the environment.
-        :return: The experience data, depending on the driver this can be e.g. trajectories or single transitions
+        :return: A list containing for every episode a dict {'s': states, 'a': actions, 'r': rewards,
+            'terminal': done_flags} where every value in the dict is a numpy array
         """
         pass
