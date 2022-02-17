d679827d1c677f37006907509f3bf15e6c250f29
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-02-17 14:52:22 +0100

    Update code to be compatible with new interface

--- training/offline_rl_driver.py
+++ training/offline_rl_driver.py
@@ -3,18 +3,30 @@ from typing import Any
 from torch.utils.data import Dataset, DataLoader
 
 from training.driver import Driver
+from memory.trajectory_memory import TrajectoryMemory
 
 
 class OfflineRLDriver(Driver):
 
-    def __init__(self, dataset: Dataset, batch_size: int, shuffle: bool = True):
+    def __init__(self,
+                 dataset: Dataset,
+                 batch_size: int,
+                 shuffle: bool = True):
         super(OfflineRLDriver, self).__init__(batch_size)
 
         self.dataset = dataset
+        self.batch_size = batch_size
         self.data_loader = DataLoader(dataset, batch_size, shuffle, num_workers=0)
         self._data_iter = iter(self.data_loader)
 
-    def interact(self) -> Any:
+    def interact(self,
+                 n_episodes: int,
+                 *args,
+                 **kwargs) -> TrajectoryMemory:
+        if n_episodes != self.batch_size:
+            raise ValueError(f'Expected n_episodes ({n_episodes}) to equal self.batch_size ({self.batch_size})')
+
         batch = next(self._data_iter)
-        return batch
+
+        return TrajectoryMemory(batch)
 
