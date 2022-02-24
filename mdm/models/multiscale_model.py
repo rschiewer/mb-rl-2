56f71a8106cc64217658ac9be2c35fed9ab0f1bb
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-02-24 19:08:27 +0100

    Add methods for single step rollout, abstract rollout and to obtain abstract model posterior steps

--- mdm/models/multiscale_model.py
+++ mdm/models/multiscale_model.py
@@ -262,6 +262,7 @@ class MultiscaleDynamicsModel(DynamicsModel):
         s, h, macro_s, macro_a, macro_r, macro_s_next, last_actions = self._gen_placeholders(d_batch)
 
         for t in range(n_steps):
+            # TODO: should this be done at time step 0 already?
             if t % self.abstract_step_size == 0:  # invoke abstract model every k time steps
                 # preparations
                 last_actions_tens = torch.stack(list(last_actions), dim=1)
@@ -407,19 +408,24 @@ class MultiscaleDynamicsModel(DynamicsModel):
 
         return compatible, tens_name
 
-    def rollout_n_step(self,
-                       start_states: torch.Tensor,
-                       actions: torch.Tensor,
-                       macro_s: torch.Tensor,
-                       macro_a: torch.Tensor,
-                       macro_r: torch.Tensor,
-                       macro_s_next: torch.Tensor,
-                       h = None):
+    def rollout_single_step(self,
+                            start_states: torch.Tensor,
+                            actions: torch.Tensor,
+                            macro_s: torch.Tensor = None,
+                            macro_a: torch.Tensor = None,
+                            macro_r: torch.Tensor = None,
+                            macro_s_next: torch.Tensor = None,
+                            h: Tuple[torch.Tensor, torch.Tensor] = None):
         d_batch, n_steps = actions.shape[:2]
         n_start_states = start_states.shape[1]
 
+        # prepare necessary placeholders
         s = torch.zeros(d_batch, self.d_state)
         h = self.single_step_model.det_mdl.gen_h_placeholder(d_batch) if h is None else h
+        macro_s = torch.zeros(d_batch, self.d_macro_state) if macro_s is None else macro_s
+        macro_a = torch.zeros(d_batch, self.d_macro_action) if macro_a is None else macro_a
+        macro_r = torch.zeros(d_batch, self.d_macro_reward) if macro_r is None else macro_r
+        macro_s_next = torch.zeros_like(macro_s) if macro_s_next is None else macro_s_next
 
         s_mem, s_dist_mem = [], []
         r_mem, r_dist_mem = [], []
@@ -437,19 +443,33 @@ class MultiscaleDynamicsModel(DynamicsModel):
             s_dist_mem.append(s_next_dist)
             r_dist_mem.append(r_next_dist)
 
+            s = s_next
+
+        r_mem = torch.stack(r_mem, dim=1)
+        s_mem = torch.stack(s_mem, dim=1)
+
+        return s_mem, s_dist_mem, r_mem, r_dist_mem, h
+
     def rollout_abstract(self,
-                         macro_start_state: torch.Tensor,
+                         macro_start_states: torch.Tensor,
                          macro_actions: torch.Tensor,
                          return_samples: bool = True):
         d_batch, n_steps = macro_actions.shape[:2]
-        macro_s = macro_start_state
+        n_start_states = macro_start_states.shape[1]
+
+        macro_s = None
 
         macro_s_prior_mem, macro_r_prior_mem = [], []
-
         for t in range(n_steps):
-            _, macro_s_next_prior, macro_r_next_prior = self.abstract_model(macro_s, macro_actions[:, t])
+            if t < n_start_states:
+                macro_s = macro_start_states[:, t]
+            macro_a = macro_actions[:, t]
+
+            _, macro_s_next_prior, macro_r_next_prior = self.abstract_model(macro_s, macro_a)
+
             macro_s_prior_mem.append(macro_s_next_prior)
             macro_r_prior_mem.append(macro_r_next_prior)
+
             macro_s = macro_s_next_prior.rsample()
 
         if return_samples:
@@ -457,6 +477,20 @@ class MultiscaleDynamicsModel(DynamicsModel):
             macro_r_prior_mem = [entry.rsample() for entry in macro_r_prior_mem]
 
         return macro_s_prior_mem, macro_r_prior_mem
+
+    def macro_next_posterior(self,
+                             macro_s: torch.Tensor,
+                             macro_a: torch.Tensor,
+                             h: Tuple[torch.Tensor, torch.Tensor],
+                             return_samples: bool = True):
+        h_flat = self._flatten_h(h)
+        _, macro_s_next_posterior, macro_r_next_posterior = self.abstract_model(macro_s, macro_a, h_flat)
+
+        if return_samples:
+            macro_s_next_posterior = macro_s_next_posterior.sample()
+            macro_r_next_posterior = macro_r_next_posterior.sample()
+
+        return macro_s_next_posterior, macro_r_next_posterior
 
     def _gen_placeholders(self, d_batch: int):
         s = torch.zeros(d_batch, self.d_state)
