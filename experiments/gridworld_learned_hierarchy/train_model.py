51d40f61c0cd8a71aee5319df0ac3c181c3a5cf2
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-03-05 13:35:38 +0100

    Adapt training script to use offline data

--- experiments/gridworld_learned_hierarchy/train_model.py
+++ experiments/gridworld_learned_hierarchy/train_model.py
@@ -5,6 +5,8 @@ import numpy as np
 from mdm.training.dynamics_model_trainer import DynamicsModelTrainer, np_one_hot
 from mdm.memory.trajectory_memory import flatten_and_unsqueeze
 from mdm.training.gym_driver import GymEpisodeDriver
+from mdm.training.offline_rl_driver import OfflineRLDriver
+from mdm.memory.trajectory_memory import TrajectoryMemory
 from mdm.models.multiscale_model import *
 from mdm.gridworld.gridworld import Gridworld
 from mdm.utils.utils import here
@@ -12,7 +14,6 @@ from mdm.utils.utils import here
 
 if __name__ == '__main__':
     env = Gridworld.from_cleartext(here() / '../../mdm/gridworld/8x8_v0.mapdata')
-    n_episodes = 1000
 
     mdl_d_state = env.observation_space.shape[0]
     mdl_d_action = env.action_space.n
@@ -24,16 +25,17 @@ if __name__ == '__main__':
     mdl_d_macro_reward = 1
     mdl_n_abstract_steps = 5
 
-    trainer_d_batch = 256
+    trainer_d_batch = 512
     trainer_n_warmup_steps = 1
     trainer_n_train_steps = 1000
-    trainer_n_eval_interval = 10
+    trainer_n_eval_interval = 50
 
-    def collect_policy(observation):
-        return env.action_space.sample()
+    train_mem = TrajectoryMemory.load(here() / 'gridworld_train.samples')
+    train_driver = OfflineRLDriver(train_mem)
+    test_mem = TrajectoryMemory.load(here() / 'gridworld_test.samples')
+    test_driver = OfflineRLDriver(test_mem)
 
-    collect_driver = GymEpisodeDriver(env, collect_policy)
-
+    # build model
     single_step_mdl = build_single_step_model(mdl_d_macro_state, mdl_d_macro_action, mdl_d_macro_reward, mdl_d_state,
                                               mdl_d_action, mdl_d_reward, mdl_d_hidden, mdl_n_rec_layers)
     abstract_mdl = build_abstract_model(mdl_d_macro_state, mdl_d_macro_action, mdl_d_macro_reward, mdl_d_hidden,
@@ -42,17 +44,19 @@ if __name__ == '__main__':
     multiscale_mdl = MultiscaleDynamicsModel(single_step_mdl, abstract_mdl, macro_action_mdl, mdl_n_abstract_steps,
                                              mdl_d_state, mdl_d_action, mdl_d_reward, mdl_d_macro_state,
                                              mdl_d_macro_action, mdl_d_macro_reward)
+    multiscale_mdl = multiscale_mdl.to('cuda')
     optimizer = torch.optim.Adam(multiscale_mdl.parameters(), lr=0.001)
 
+    # train model
     def get_batch_train():
-        s, a, r, terminal = collect_driver.interact(trainer_d_batch).to_np_arrays(dtype=np.float32)
+        s, a, r, terminal = train_driver.interact(trainer_d_batch).to_np_arrays(dtype=np.float32)
         s, a, r, terminal = flatten_and_unsqueeze(s, a, r, terminal)
         s /= (env.grid_h - 1, env.grid_w - 1)
         a = np_one_hot(a.astype(np.int64), n_categories=mdl_d_action)
         return s, a, r, terminal
 
     def get_batch_test():
-        s, a, r, terminal = collect_driver.interact(trainer_d_batch).to_np_arrays(dtype=np.float32)
+        s, a, r, terminal = test_driver.interact(trainer_d_batch).to_np_arrays(dtype=np.float32)
         s, a, r, terminal = flatten_and_unsqueeze(s, a, r, terminal)
         s /= (env.grid_h - 1, env.grid_w - 1)
         a = np_one_hot(a.astype(np.int64), n_categories=mdl_d_action)
