05d6dda1d79fc84b42a6486a4bd34763214df156
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-04-24 12:23:32 +0200

    Reorganization of code/inheritance structures

--- mdm/utils/torch_tools.py
+++ mdm/utils/torch_tools.py
@@ -69,12 +69,63 @@ class RecurrentBlock(torch.nn.Module, DeviceMixin):
                 torch.zeros(self.n_layers, d_batch, self.d_hidden, device=d))
 
 
-class GaussianBlock(torch.nn.Module, DeviceMixin):
+class FeedforwardBlock(torch.nn.Module, DeviceMixin):
 
     def __init__(self,
                  *d_inputs: int,
                  lws: Union[Iterable[int], int]):
-        super(GaussianBlock, self).__init__()
+        super(FeedforwardBlock, self).__init__()
+
+        if type(lws) is int:
+            lws = [lws]
+
+        self.d_inputs = d_inputs
+        self.lws = (sum(d_inputs), *lws)
+        self.layer_list = torch.nn.ModuleList([torch.nn.Linear(lw_in, lw_out)
+                                               for lw_in, lw_out in zip(self.lws, self.lws[1:])])
+
+    def forward(self,
+                *xs: torch.Tensor):
+        x = torch.concat(xs, dim=-1)
+        for l in self.layer_list[:-1]:
+            x = l(x)
+            x = torch.nn.functional.gelu(x)
+        x = self.layer_list[-1](x)
+
+        return x
+
+
+class GaussianBlock(FeedforwardBlock):
+
+    def __init__(self,
+                 *d_inputs: int,
+                 lws: Union[Iterable[int], int]):
+        if type(lws) is int:
+            lws = [lws]
+        lws = (*lws[:-1], lws[-1] * 2)  # double last layer to have params for loc and scale
+
+        super(GaussianBlock, self).__init__(*d_inputs, lws=lws)
+
+    def forward(self,
+                *xs: torch.Tensor):
+        x = super().forward(*xs)
+        mu, logvar = torch.tensor_split(x, 2, dim=-1)
+        #std = logvar.exp().pow(0.5) + 1.0
+        #std = torch.log(1 + logvar.exp()) + 1e-1
+        #std = torch.nn.functional.relu(logvar) + 0.01
+        #std = torch.distributions.transform_to(torch.distributions.Normal.arg_constraints['scale'])(logvar) + 0.01
+        std = torch.abs(logvar) + 0.01
+        x_dist = torch.distributions.Normal(mu, std)
+
+        return x_dist
+
+
+class GaussianBlock_old(torch.nn.Module, DeviceMixin):
+
+    def __init__(self,
+                 *d_inputs: int,
+                 lws: Union[Iterable[int], int]):
+        super(GaussianBlock_old, self).__init__()
 
         if type(lws) is int:
             lws = [lws]
@@ -101,34 +152,6 @@ class GaussianBlock(torch.nn.Module, DeviceMixin):
         x_dist = torch.distributions.Normal(mu, std)
 
         return x_dist
-
-
-class FeedforwardBlock(torch.nn.Module, DeviceMixin):
-
-    def __init__(self,
-                 *d_inputs: int,
-                 lws: Union[Iterable[int], int] = None):
-        super(FeedforwardBlock, self).__init__()
-
-        if lws is None:
-            lws = []
-        elif type(lws) is int:
-            lws = [lws]
-
-        self.d_inputs = d_inputs
-        self.lws = (sum(d_inputs), *lws)
-        self.layer_list = torch.nn.ModuleList([torch.nn.Linear(lw_in, lw_out)
-                                               for lw_in, lw_out in zip(self.lws, self.lws[1:])])
-
-    def forward(self,
-                *xs: torch.Tensor):
-        x = torch.concat(xs, dim=-1)
-        for l in self.layer_list[:-1]:
-            x = l(x)
-            x = torch.nn.functional.gelu(x)
-        x = self.layer_list[-1](x)
-
-        return x
 
 
 class ContinuousBernoulliBlock(FeedforwardBlock):
