8e4a4b0d350ec6608de0a28ca78e0743a9015b1e
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-06-10 14:47:16 +0200

    Add layer norm parameter to AbstractActionModel

--- mdm/models/building_blocks.py
+++ mdm/models/building_blocks.py
@@ -14,12 +14,13 @@ class AbstractActionModel(torch.nn.Module):
                  abstract_step_size: int,
                  d_abstract_action: int,
                  lws: tuple = (64, 64),
+                 layer_norm: bool = False,
                  activation: str = 'relu'):
         super(AbstractActionModel, self).__init__()
 
         lws = (d_action * abstract_step_size, *lws, d_abstract_action)
         self.flatten_layer = torch.nn.Flatten(start_dim=1)
-        self.det_mdl = torch.nn.Sequential(*layers_with_activation(lws, activation))
+        self.det_mdl = torch.nn.Sequential(*layers_with_activation(lws, activation, layer_norm=layer_norm))
 
     def forward(self,
                 actions: torch.Tensor) -> torch.Tensor:
