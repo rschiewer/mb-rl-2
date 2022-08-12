0d1401b8cfae0639d69cf5a9fa946e34d8078c08
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-08-12 14:40:20 +0200

    Rework plotting for better performance

--- experiments/gridworld_learned_hierarchy/gen_prim_state_plots.py
+++ experiments/gridworld_learned_hierarchy/gen_prim_state_plots.py
@@ -9,6 +9,7 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
 import matplotlib as mpl
 import seaborn as sns; sns.set_theme()
 from mpl_toolkits.axes_grid1 import ImageGrid
@@ -23,7 +24,7 @@ from mdm.memory.trajectory_memory import flatten_and_unsqueeze
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Provide neptune run_id for loading the correct model')
-    parser.add_argument('id', type=str, nargs=1)
+    parser.add_argument('id', type=str)
     parser.add_argument('-log', action='store_true')
     args = parser.parse_args()
 
@@ -33,50 +34,60 @@ if __name__ == '__main__':
     if len(args.id) == 0:
         model_path = f'{cfg["final_model_path"]}.ptmdl'
     else:
-        model_path = f'{cfg["final_model_path"]}_{args.id[0]}.ptmdl'
+        model_path = f'{cfg["final_model_path"]}_{args.id}.ptmdl'
 
     env = Gridworld.from_cleartext(here() / '../../mdm/gridworld/8x8_v0.mapdata')
     mdl: MultiscaleDynamicsModelMK2 = torch.load(here() / model_path)
 
     maps_s_mean, maps_s_std = [], []
-    for n_step in range(2):
+    for n_step in range(5):
         _map_s_mean, _map_s_std = gen_prim_state_map(env, mdl, n_step)
         maps_s_mean.append(_map_s_mean)
         maps_s_std.append(_map_s_std)
-    maps_s_mean = np.stack(maps_s_mean, axis=1)
-    maps_s_std = np.stack(maps_s_std, axis=1)
+    maps_s_mean = np.stack(maps_s_mean, axis=0)
+    maps_s_std = np.stack(maps_s_std, axis=0)
 
+    #v_min = np.minimum(maps_s_mean.min(), maps_s_std.min())
+    #v_max = np.maximum(maps_s_mean.max(), maps_s_std.max())
     all_values = np.concatenate([maps_s_mean, maps_s_std])
     v_min = all_values.min()
     v_max = all_values.max()
 
-    max_n_cols = min(len(maps_s_mean), 4)
-    n_rows = ceil(len(maps_s_mean) / max_n_cols)
-    fig = plt.figure(constrained_layout=True, figsize=(20, 20))
-    subfigs = fig.subfigures(n_rows, max_n_cols, wspace=0.1, hspace=0.2)
-    animations = []
-    for i, (subfig, _map_s_mean, _map_s_std) in enumerate(zip(subfigs.flat, maps_s_mean, maps_s_std)):
-        subfig.suptitle(f'Component {i}')
-        axes = subfig.subplots(1, 2)
-        axes[0].grid(False)
-        axes[1].grid(False)
-        axes[0].set_title('mean')
-        axes[1].set_title('std')
-        axes[0].axis('off')
-        axes[1].axis('off')
+    max_n_cols = min(maps_s_mean.shape[1], 4)
+    n_rows = ceil(maps_s_mean.shape[1] / max_n_cols)
+    fig = plt.figure( figsize=(20, 20))
+    gs = gridspec.GridSpec(n_rows, max_n_cols, wspace=0.2, hspace=0.2, figure=fig, left=0.01, right=0.99, bottom=0.01,
+                           top=0.99)
+    subfig_axes = []
+    for i, subfig in enumerate(gs):
+        subfig_grid = gridspec.GridSpecFromSubplotSpec(1, 2, hspace=0.1, subplot_spec=subfig)
+        axes_0 = fig.add_subplot(subfig_grid[0, 0])
+        axes_1 = fig.add_subplot(subfig_grid[0, 1])
+        axes_0.grid(False)
+        axes_1.grid(False)
+        axes_0.set_title('mean')
+        axes_1.set_title('std')
+        axes_0.axis('off')
+        axes_1.axis('off')
+        subfig_axes.append([axes_0, axes_1])
 
-        ims = []
-        for t, (frame_mean, frame_std) in enumerate(zip(_map_s_mean, _map_s_std)):
+    artists = []
+    for t, (frames_mean, frames_std) in enumerate(zip(maps_s_mean, maps_s_std)):
+        current_frame = []
+        for axes, frame_mean, frame_std in zip(subfig_axes, frames_mean, frames_std):
             im_0 = axes[0].matshow(frame_mean, vmin=v_min, vmax=v_max)
             im_1 = axes[1].matshow(frame_std, vmin=v_min, vmax=v_max)
             label = axes[0].text(0.01, 0.01, f'{t}', transform=axes[0].transAxes, color='green')
-            ims.append([im_0, im_1, label])
-        subfig.colorbar(im_0, ax=axes.ravel().tolist())
-        ani = animation.ArtistAnimation(subfig, ims, interval=200, blit=True, repeat_delay=0)
-        animations.append(ani)
-        #subfig.set_facecolor('0.8')
-    writer = animation.FFMpegWriter(fps=60)
-    plt.show()
+            current_frame += [im_0, im_1, label]
+            if t == 0:
+                fig.colorbar(im_0, ax=axes)
+        artists.append(current_frame)
+
+    ani = animation.ArtistAnimation(fig, artists, interval=500, blit=True, repeat_delay=2000)
+    plt.tight_layout()
+    #plt.show()
+    writer = animation.PillowWriter(fps=10)
+    ani.save(f'prim_state_plot_{args.id}.gif', writer=writer)
 
 
 
