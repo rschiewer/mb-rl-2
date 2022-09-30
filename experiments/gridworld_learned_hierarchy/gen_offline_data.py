eb12d624f5481085940ca0c42eefe026ab2b800c
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-09-30 15:57:13 +0200

    Add code to create non-disjunct train and test sets

--- experiments/gridworld_learned_hierarchy/gen_offline_data.py
+++ experiments/gridworld_learned_hierarchy/gen_offline_data.py
@@ -70,14 +70,21 @@ if __name__ == '__main__':
     train_mem = collect_driver.interact(n_episodes_train, True)
 
     train_mem_cleaned = remove_duplicates_mp(train_mem, n_proc=14)
-    #train_mem_cleaned_2 = remove_duplicates(train_mem)
     print(f'Removed {len(train_mem) - len(train_mem_cleaned)} duplicate trajectories from sample memory.')
+
+    # for testing only
+    #train_mem_cleaned_2 = remove_duplicates(train_mem)
     #print(f'Removed {len(train_mem) - len(train_mem_cleaned_2)} duplicate trajectories from sample memory.')
 
     train_mem_cleaned = train_mem_cleaned.shuffle()
     n_episodes_test = round(len(train_mem_cleaned) * perc_test)
     train_mem = train_mem_cleaned[n_episodes_test:]
     test_mem = train_mem_cleaned[:n_episodes_test]
+
+    #n_episodes_test = round(len(train_mem) * perc_test)
+    #trai_mem = train_mem.shuffle()
+    #train_mem = train_mem[n_episodes_test:]
+    #test_mem = train_mem[:n_episodes_test]
 
     TrajectoryMemory.store(train_mem, here() / 'gridworld_train.samples')
     TrajectoryMemory.store(test_mem, here() / 'gridworld_test.samples')
