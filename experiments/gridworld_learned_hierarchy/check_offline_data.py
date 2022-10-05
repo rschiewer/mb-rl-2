906db06d8e72407266fd0beca0e7d8ff8696ecb3
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-10-05 12:16:19 +0200

    Update code to new trajectory format including weights

--- experiments/gridworld_learned_hierarchy/check_offline_data.py
+++ experiments/gridworld_learned_hierarchy/check_offline_data.py
@@ -7,13 +7,13 @@ from mdm.utils.utils import here
 
 if __name__ == '__main__':
     env = Gridworld.from_cleartext(here() / '../../mdm/gridworld/8x8_v0.mapdata')
-    train_mem = TrajectoryMemory.load(here() / 'gridworld_train.samples').shuffle()
-    test_mem = TrajectoryMemory.load(here() / 'gridworld_test.samples').shuffle()
+    train_mem = TrajectoryMemory.load(here() / 'gridworld_train.samples')
+    test_mem = TrajectoryMemory.load(here() / 'gridworld_test.samples')
 
     for traj in tqdm(train_mem, desc='Checking train samples'):
-        s, a, r, terminal = traj.values()
+        s, a, r, terminal, w = traj.values()
         env.enact_sequence(s, a, r, terminal, False, 0)
 
     for traj in tqdm(test_mem, desc='Checking test samples'):
-        s, a, r, terminal = traj.values()
+        s, a, r, terminal, w = traj.values()
         env.enact_sequence(s, a, r, terminal, False, 0)
