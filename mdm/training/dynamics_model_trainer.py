286a699033934148457f82e8736407e5dc018eee
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-10-17 10:55:10 +0200

    Disable local model checkpointing for now since training multiple models in parallel does interfere with this anyway

--- mdm/training/dynamics_model_trainer.py
+++ mdm/training/dynamics_model_trainer.py
@@ -85,8 +85,8 @@ class DynamicsModelTrainer(ABC):
                 eval_losses = self.model.eval_step(s, a, r, term)
 
                 last_total_loss = last_eval_losses.get('total', np.inf)
-                if checkpoint_path and eval_losses['total'] < last_total_loss:
-                    torch.save(self.model, Path(checkpoint_path).parent / 'checkpoint.ptmdl')
+                #if checkpoint_path and eval_losses['total'] < last_total_loss:
+                #    torch.save(self.model, Path(checkpoint_path).parent / 'checkpoint.ptmdl')
 
                 last_eval_losses = eval_losses
 
