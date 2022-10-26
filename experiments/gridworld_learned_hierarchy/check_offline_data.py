15ba2767368f17ff261ff89c376f53398a46ec22
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-10-26 10:22:46 +0200

    Add trajectory statistics plots

--- experiments/gridworld_learned_hierarchy/check_offline_data.py
+++ experiments/gridworld_learned_hierarchy/check_offline_data.py
@@ -2,14 +2,19 @@ from tqdm import tqdm
 
 from mdm.gridworld.gridworld import Gridworld
 from mdm.memory.trajectory_memory import TrajectoryMemory
-from mdm.utils.utils import here
+from mdm.utils.utils import here, load_yaml
 
 
 if __name__ == '__main__':
+    cfg = load_yaml(here() / 'model_cfg.yaml')
+
     map_version = 'v0'
     env = Gridworld.from_cleartext(here() / f'../../mdm/gridworld/8x8_{map_version}.mapdata')
-    train_mem = TrajectoryMemory.load(here() / 'gridworld_train.samples')
-    test_mem = TrajectoryMemory.load(here() / 'gridworld_test.samples')
+    train_mem = TrajectoryMemory.load(here() / cfg['train_samples'])
+    test_mem = TrajectoryMemory.load(here() / cfg['test_samples'])
+
+    train_mem.plot_stats(bins=20)
+    test_mem.plot_stats(bins=20)
 
     for traj in tqdm(train_mem, desc='Checking train samples'):
         s, a, r, terminal, w = traj.values()
@@ -18,3 +23,5 @@ if __name__ == '__main__':
     for traj in tqdm(test_mem, desc='Checking test samples'):
         s, a, r, terminal, w = traj.values()
         env.enact_sequence(s, a, r, terminal, False, 0)
+
+
