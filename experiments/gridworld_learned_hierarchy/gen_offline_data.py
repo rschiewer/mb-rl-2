55675ae65ff9b942ce7da27541beff5598a0d6d2
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-10-28 14:40:51 +0200

    Add simple tabular q-learning agent to collect high quality data if needed

--- experiments/gridworld_learned_hierarchy/gen_offline_data.py
+++ experiments/gridworld_learned_hierarchy/gen_offline_data.py
@@ -1,11 +1,70 @@
 from multiprocessing import Pool
 
 from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
 
 from mdm.training.gym_driver import GymEpisodeDriver
 from mdm.memory.trajectory_memory import TrajectoryMemory
 from mdm.gridworld.gridworld import Gridworld
 from mdm.utils.utils import here
+
+
+class TabularQLearningPolicy:
+
+    def __init__(self, grid_w: int, grid_h: int, n_actions: int, alpha: float, gamma:float, epsilon: float):
+        self.q = np.zeros((grid_w, grid_h, n_actions), dtype=np.float32)
+        self.n_actions = n_actions
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+    def _update(self, s_t, a_t, r_t, term_t, s_tt):
+        s_t = tuple(s_t)
+        s_tt = tuple(s_tt)
+        target = r_t
+        if not term_t:  # unnecessary for q initialized to zeros
+            target += self.gamma * self.q[s_tt].max()
+
+        q_new = (1 - self.alpha) * self.q[s_t][a_t] + self.alpha * target
+        delta = np.abs(self.q[s_t][a_t] - q_new)
+        self.q[s_t][a_t] = q_new
+        return delta
+
+    def _update_vec(self, s_t, a_t, r_t, term_t, s_tt):
+        target = r_t + self.gamma * self.q[s_tt[:, 0], s_tt[:, 1]].max(axis=-1)
+        new_q = (1 - self.alpha) * self.q[s_t[:, 0], s_t[:, 1], a_t] + self.alpha * target
+        diff = np.sum(np.abs(self.q[s_t[:, 0], s_t[:, 1], a_t] - new_q))
+        self.q[s_t[:, 0], s_t[:, 1], a_t] = new_q
+        return diff
+
+    def train(self, mem: TrajectoryMemory, d_batch: int):
+        if len(mem) == 0:
+            return
+
+        batch = mem.sample(d_batch, False)
+        s, a, r, terminal, w = batch.to_np_arrays(dtype=(int, int, float, bool, float))
+        delta = 0
+
+        for s_t, a_t, r_t, term_t, s_tt in zip(s[:, :-1], a[:, 1:], r[:, 1:], terminal[:, 1:], s[:, 1:]):
+            delta += self._update_vec(s_t, a_t, r_t, term_t, s_tt)
+
+        return delta / d_batch
+
+        for n in range(d_batch):
+            for s_t, a_t, r_t, term_t, s_tt in zip(s[n, :-1], a[n, 1:], r[n, 1:], terminal[n, 1:], s[n, 1:]):
+                delta += self._update(s_t, a_t, r_t, term_t, s_tt)
+                if term_t:
+                    break
+        return delta / d_batch
+
+    def decide(self, o):
+        if np.random.rand() < self.epsilon:
+            a = np.random.choice(range(self.n_actions))
+        else:
+            aa = self.q[tuple(o.astype(int))]
+            a = np.argmax(np.random.random(aa.shape) * (aa == aa.max()))
+        return a
 
 
 class TrajectoryIsUnique:
@@ -59,17 +118,35 @@ def remove_duplicates_mp(mem: TrajectoryMemory, n_proc: int = 10):
 
 
 if __name__ == '__main__':
-    map_version = 'v1'
+    map_version = 'v2'
     env = Gridworld.from_cleartext(here() / f'../../mdm/gridworld/8x8_{map_version}.mapdata')
-    n_episodes_train = 50000
+    n_episodes_train = 20000
     perc_test = 0.1
     disjunct_train_test = False
+    expert_trajectories = True
 
-    def collect_policy(*args):
-        return env.action_space.sample()
+    train_mem = TrajectoryMemory()
+    if expert_trajectories:
+        agent = TabularQLearningPolicy(env.grid_h, env.grid_w, env.action_space.n, 0.1, 0.99, 0.1)
+
+        def collect_policy(o, r, term, i_ep):
+            a = agent.decide(o)
+            if i_ep % 100 == 0:
+                last_err = agent.train(train_mem, 128)
+                #print(last_err)
+            return a
+    else:
+        agent = None
+
+        def collect_policy(*args):
+            return env.action_space.sample()
 
     collect_driver = GymEpisodeDriver(env, collect_policy)
-    train_mem = collect_driver.interact(n_episodes_train, True)
+    train_mem = collect_driver.interact(n_episodes_train, True, train_mem)
+
+    if agent:
+        plt.matshow(agent.q.max(axis=2))
+        plt.show()
 
     if disjunct_train_test:
         train_mem = remove_duplicates_mp(train_mem, n_proc=14)
