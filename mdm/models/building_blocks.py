5604d1827484cd3cc93eb5569d77914460514327
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-11-03 15:03:45 +0100

    Implement different latent distribution types for the RSSM and the usage of named torch.nn.Sequential submodules for easier identification during debugging

--- mdm/models/building_blocks.py
+++ mdm/models/building_blocks.py
@@ -1,12 +1,16 @@
 from typing import Tuple, Optional, Sequence, Union, TypeVar
+from abc import ABC, abstractmethod
+from itertools import product
 from enum import Enum
 
 import torch
+import numpy as np
 #import haste_pytorch as haste
 #from RIM import RIM
 
-from mdm.utils.torch_tools import (layers_with_activation, add_time_dim, remove_time_dim, sample_from_gaussian,
-                                   make_gaussian_params, get_mu, get_sigma, RnnStateType, sample_from_categorical)
+from mdm.utils.torch_tools import (layers_with_activation as lwa, add_time_dim, remove_time_dim, sample_from_gaussian,
+                                   make_gaussian_params, get_mu, get_sigma, RnnStateType, sample_from_categorical,
+                                   sample_from_bernoulli, make_bernoulli_params, build_bernoulli, build_categorical)
 from mdm.utils.utils import DistributionType
 
 
@@ -22,8 +26,10 @@ class AbstractActionModel(torch.nn.Module):
                  distribution_type: str = None):
         super(AbstractActionModel, self).__init__()
 
+        self.d_a = d_a
         self.d_abstract_action = d_a_abstract
         self.distribution_type = distribution_type
+        self.abstract_step_size = abstract_step_size
 
         self.flatten_layer = torch.nn.Flatten(start_dim=1)
         if distribution_type is None:
@@ -38,7 +44,7 @@ class AbstractActionModel(torch.nn.Module):
         else:
             raise ValueError(f'Unsupported distribution type: {distribution_type}')
 
-        self.det_mdl = torch.nn.Sequential(*layers_with_activation(lws, activation, layer_norm=layer_norm))
+        self.det_mdl = torch.nn.Sequential(*lwa(lws, activation, layer_norm=layer_norm))
 
     @staticmethod
     def _prob_mdl_none(x: torch.Tensor, sample: bool):
@@ -70,6 +76,16 @@ class AbstractActionModel(torch.nn.Module):
     def forward(self,
                 actions: torch.Tensor,
                 sample: bool = True) -> torch.Tensor:
+        x2 = actions.argmax(dim=-1)
+        exp_mat = torch.full_like(x2, actions.shape[-1])
+        exp_mat = torch.cumprod(exp_mat, dim=1)
+        exp_mat = torch.flip(exp_mat, dims=(1,))
+        exp_mat = torch.roll(exp_mat, -1, dims=1)
+        exp_mat[:, -1] = 1
+        x2 = torch.sum(x2 * exp_mat, dim=1)
+        x2 = torch.nn.functional.one_hot(x2, num_classes=self.d_abstract_action).to(torch.float32)
+        return x2
+
         # actions.shape = (d_batch, n_abstract_steps, d_action)
         x = self.flatten_layer(actions)
         x = self.det_mdl(x)
@@ -98,6 +114,7 @@ class RSSM(torch.nn.Module):
                  layer_norm: bool = False,
                  activation: str = 'relu',
                  rnn_type: str = 'lstm',
+                 latent_dist: str = 'normal',
                  stochastic_outputs: bool = True):
         super().__init__()
 
@@ -113,14 +130,32 @@ class RSSM(torch.nn.Module):
         self.layer_norm = layer_norm
         self.activation = activation
         self.rnn_type = rnn_type
+        self.latent_dist = latent_dist
         self.stochastic_outputs = stochastic_outputs
 
-        z_prior_lws = (d_h, *z_prior_lws, d_z * 2)
-        z_post_lws = (d_h + d_z * 2 + d_x_posterior, *z_post_lws, d_z * 2)
-        o_lws = (d_h + d_z, *o_lws, d_o * 2)
-        r_lws = (d_h + d_z, *r_lws, d_r * 2)
-        term_lws = (d_h + d_z, *term_lws, 1)
-        z_preproc_lws = (d_z, d_z, d_z)
+        self.n_latent_categories = 8
+        if latent_dist == 'normal':
+            d_z_post_in = d_h + d_z * 2
+            d_z_final = d_z * 2
+            d_z_smpl = d_z
+        elif latent_dist == 'bernoulli':
+            d_z_post_in = d_h + d_z
+            d_z_final = d_z
+            d_z_smpl = d_z
+        elif latent_dist == 'categorical':
+            d_z_post_in = d_h + d_z * self.n_latent_categories
+            d_z_final = d_z * self.n_latent_categories
+            d_z_smpl = d_z * self.n_latent_categories
+        else:
+            raise ValueError(f'Unknown latent distribution type: {latent_dist}')
+        self.d_z_smpl = d_z_smpl
+
+        z_prior_lws = (d_h, *z_prior_lws, d_z_final)
+        z_post_lws = (d_z_post_in + d_x_posterior, *z_post_lws, d_z_final)
+        o_lws = (d_h + d_z_smpl, *o_lws, d_o * 2)
+        r_lws = (d_h + d_z_smpl, *r_lws, d_r * 2)
+        term_lws = (d_h + d_z_smpl, *term_lws, 1)
+        z_preproc_lws = (d_z_smpl, d_z_smpl, d_z_smpl)
 
         if rnn_type == 'lstm':
             rnn_constr = torch.nn.LSTM
@@ -129,7 +164,7 @@ class RSSM(torch.nn.Module):
         else:
             raise ValueError(f'Unsupported rnn type: {rnn_type}')
 
-        d_det_core = d_z + d_a + d_ctx_high_level
+        d_det_core = d_z_smpl + d_a + d_ctx_high_level
         self._rnn = rnn_constr(d_det_core, hidden_size=d_h, num_layers=n_hidden_layers, batch_first=True,
                                dropout=hidden_dropout)
         #self._rnn = haste.IndRNN(d_det_core, hidden_size=d_hidden, batch_first=True)
@@ -138,12 +173,12 @@ class RSSM(torch.nn.Module):
         #self._rnn = RIM(device='cuda', input_size=d_det_core, hidden_size=d_hidden, num_units=n_units, k=3,
         #                n_layers=n_hidden_layers, rnn_cell='LSTM', bidirectional=False)
 
-        self._z_preproc = torch.nn.Sequential(*layers_with_activation(z_preproc_lws, activation, layer_norm=layer_norm))
-        self._s_prior = torch.nn.Sequential(*layers_with_activation(z_prior_lws, activation, layer_norm=layer_norm))
-        self._s_post = torch.nn.Sequential(*layers_with_activation(z_post_lws, activation, layer_norm=layer_norm))
-        self._o_dist = torch.nn.Sequential(*layers_with_activation(o_lws, activation, layer_norm=layer_norm))
-        self._r_dist = torch.nn.Sequential(*layers_with_activation(r_lws, activation, layer_norm=layer_norm))
-        self._term_dist = torch.nn.Sequential(*layers_with_activation(term_lws, activation, layer_norm=layer_norm))
+        #self._z_preproc = torch.nn.Sequential(*layers_with_activation(z_preproc_lws, activation, layer_norm=layer_norm))
+        self._s_prior = torch.nn.Sequential(lwa(z_prior_lws, activation, layer_norm=layer_norm, name='z_prior'))
+        self._s_post = torch.nn.Sequential(lwa(z_post_lws, activation, layer_norm=layer_norm, name='z_post'))
+        self._o_dist = torch.nn.Sequential(lwa(o_lws, activation, layer_norm=layer_norm, name='o_dist'))
+        self._r_dist = torch.nn.Sequential(lwa(r_lws, activation, layer_norm=layer_norm, name='r_dist'))
+        self._term_dist = torch.nn.Sequential(lwa(term_lws, activation, layer_norm=layer_norm, name='term_dist'))
 
     @property
     def top_node(self):
@@ -177,7 +212,7 @@ class RSSM(torch.nn.Module):
     def zero_z(self,
                d_batch: int,
                device: torch.device) -> torch.Tensor:
-        return torch.zeros(d_batch, self.d_z, device=device)
+        return torch.zeros(d_batch, self.d_z_smpl, device=device)
 
     def zero_o(self,
                d_batch: int,
@@ -223,8 +258,8 @@ class RSSM(torch.nn.Module):
                   rnn_state: torch.Tensor,
                   a: torch.Tensor,
                   ctx_high_level: torch.Tensor):
-        z_processed = self._z_preproc(z)
-        inp = torch.concat([z_processed, a, ctx_high_level], dim=-1)
+        #z_processed = self._z_preproc(z)
+        inp = torch.concat([z, a, ctx_high_level], dim=-1)
         inp = add_time_dim(inp)
         x_det, next_rnn_state = self._rnn(inp, rnn_state)
         x_det = remove_time_dim(x_det)
@@ -246,10 +281,23 @@ class RSSM(torch.nn.Module):
             z_dist = z_post
         else:
             z_dist = z_prior
+
         if sample:
-            z_smpl = sample_from_gaussian(z_dist)
+            if self.latent_dist == 'normal':
+                z_smpl = sample_from_gaussian(z_dist)
+            elif self.latent_dist == 'bernoulli':
+                z_smpl = sample_from_bernoulli(z_dist)
+            elif self.latent_dist == 'categorical':
+                z_smpl = sample_from_categorical(z_dist)
+                z_smpl = torch.flatten(z_smpl, start_dim=1)
         else:
-            z_smpl = get_mu(z_dist)
+            if self.latent_dist == 'normal':
+                z_smpl = get_mu(z_dist)
+            elif self.latent_dist == 'bernoulli':
+                z_smpl = build_bernoulli(z_dist).probs
+            elif self.latent_dist == 'categorical':
+                z_smpl = build_categorical(z_dist).probs
+                z_smpl = torch.flatten(z_smpl, start_dim=1)
 
         #h = torch.zeros_like(h)
         s = torch.concat([h, z_smpl], dim=-1)
@@ -270,18 +318,33 @@ class RSSM(torch.nn.Module):
 
     def build_z_prior(self,
                       h: torch.Tensor) -> torch.Tensor:
-        s_prior_params = self._s_prior(h)
-        s_prior_params = make_gaussian_params(s_prior_params, self.epsilon)
-        return s_prior_params
+        z_prior_params = self._s_prior(h)
+        if self.latent_dist == 'normal':
+            z_prior_params = make_gaussian_params(z_prior_params, self.epsilon)
+        elif self.latent_dist == 'bernoulli':
+            z_prior_params = make_bernoulli_params(z_prior_params)
+        elif self.latent_dist == 'categorical':
+            z_prior_params = z_prior_params.reshape((z_prior_params.shape[0], self.d_z, self.n_latent_categories))
+        return z_prior_params
 
     def build_z_post(self,
                      h: torch.Tensor,
                      z_prior: torch.Tensor,
                      x_posterior: torch.Tensor) -> torch.Tensor:
-        inp = torch.concat([h, z_prior, x_posterior], dim=-1)
-        s_post_params = self._s_post(inp)
-        s_post_params = make_gaussian_params(s_post_params, self.epsilon)
-        return s_post_params
+        if self.latent_dist == 'normal':
+            inp = torch.concat([h, z_prior, x_posterior], dim=-1)
+            z_post_params = self._s_post(inp)
+            z_post_params = make_gaussian_params(z_post_params, self.epsilon)
+        elif self.latent_dist == 'bernoulli':
+            inp = torch.concat([h, z_prior, x_posterior], dim=-1)
+            z_post_params = self._s_post(inp)
+            z_post_params = make_bernoulli_params(z_post_params)
+        elif self.latent_dist == 'categorical':
+            z_prior = torch.flatten(z_prior, start_dim=1)
+            inp = torch.concat([h, z_prior, x_posterior], dim=-1)
+            z_post_params = self._s_post(inp)
+            z_post_params = z_post_params.reshape(z_post_params.shape[0], self.d_z, self.n_latent_categories)
+        return z_post_params
 
     def _build_o_dist(self,
                       s: torch.Tensor) -> torch.Tensor:
@@ -304,3 +367,79 @@ class RSSM(torch.nn.Module):
         #term_smpl = term_dist.rsample() if sample else term_params
         #return term_dist, term_smpl
         return torch.sigmoid(term_params)
+
+
+class ObsEncoder(torch.nn.Module, ABC):
+
+    def __init__(self, o_shape: Union[int, Sequence[int]], d_o_encoded: int):
+        super(ObsEncoder, self).__init__()
+        if isinstance(o_shape, int):
+            o_shape = (o_shape, )
+        self.o_shape = tuple(o_shape)
+        self.d_o_encoded = d_o_encoded
+
+    @abstractmethod
+    def forward(self, o: torch.Tensor):
+        pass
+
+
+class ObsDecoder(torch.nn.Module, ABC):
+
+    def __init__(self, o_shape: Union[int, Sequence[int]], d_o_encoded: int):
+        super(ObsDecoder, self).__init__()
+        if isinstance(o_shape, int):
+            o_shape = (o_shape, )
+        self.o_shape = tuple(o_shape)
+        self.d_o_encoded = d_o_encoded
+
+    @abstractmethod
+    def forward(self, o_enc: torch.Tensor):
+        pass
+
+
+class OneHotObsEncoder(ObsEncoder):
+
+    def __init__(self,
+                 o_shape: Union[int, Sequence[int]],
+                 n_categories: int,
+                 d_o_encoded: int,
+                 lws: Sequence[int],
+                 activation: str, layer_norm: bool):
+        super(OneHotObsEncoder, self).__init__(o_shape, d_o_encoded)
+        lws = (np.prod(o_shape) * n_categories, *lws, d_o_encoded)
+        self._mdl = torch.nn.Sequential(*layers_with_activation(lws, activation, layer_norm=layer_norm))
+        self.n_categories = n_categories
+
+    def forward(self,
+                o: torch.Tensor):
+        #o = torch.nn.functional.one_hot(o, n_categories=self.o_shape)
+        o = torch.flatten(o, start_dim=2)
+        return self._mdl(o)
+
+
+class OneHotObsDecoder(ObsDecoder):
+
+    def __init__(self,
+                 o_shape: Union[int, Sequence[int]],
+                 n_categories: int,
+                 d_o_encoded: int,
+                 lws: Sequence[int],
+                 activation: str,
+                 layer_norm: bool,
+                 stochastic: bool):
+        super(OneHotObsDecoder, self).__init__(o_shape, d_o_encoded)
+        lws = (d_o_encoded, *lws, np.prod(self.o_shape) * n_categories)
+        self._mdl = torch.nn.Sequential(*layers_with_activation(lws, activation, layer_norm=layer_norm))
+        self.n_categories = n_categories
+        self.stochastic = stochastic
+
+    def forward(self, o_enc: torch.Tensor):
+        o_dec = self._mdl(o_enc)
+        o_dec = o_dec.reshape((o_enc.shape[0], *self.o_shape, self.n_categories))
+        if self.stochastic:
+            return torch.distributions.RelaxedOneHotCategorical(0.1, logits=o_dec).rsample()
+        else:
+            return torch.nn.functional.softmax(o_dec, dim=-1)
+
+
+
