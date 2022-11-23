c51e101f23d786e4e440ab879ccfeb640699587e
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-11-23 14:54:37 +0100

    Adapt to new convention of using simple list as memory

--- experiments/gridworld_learned_hierarchy/gen_good_trajectories.py
+++ experiments/gridworld_learned_hierarchy/gen_good_trajectories.py
@@ -1,7 +1,7 @@
 from mdm.training.gym_driver import GymEpisodeDriver
 from mdm.memory.trajectory_memory import TrajectoryMemory
 from mdm.gridworld.gridworld import Gridworld, FullyObservableGridworld
-from mdm.utils.utils import here, compute_returns
+from mdm.utils.utils import here, store_memory
 
 if __name__ == '__main__':
     map_version = 'v0'
@@ -17,14 +17,10 @@ if __name__ == '__main__':
     actions += [1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1]
     a_iter = iter(actions)
 
-    def collect_policy(observation, reward, terminal, i_ep):
+    def collect_policy(*args):
         return next(a_iter)
 
     collect_driver = GymEpisodeDriver(env, collect_policy)
     mem = collect_driver.interact(5, True)
-    compute_returns(mem, 1.0)
 
-    for traj in mem:
-        print(traj['w'])
-
-    TrajectoryMemory.store(mem, here() / f'gridworld_{map_version}_good_trajectories.samples')
+    store_memory(mem, here() / f'gridworld_{map_version}_good_trajectories.samples')
