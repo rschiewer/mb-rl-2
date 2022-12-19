148179751cee1fe0af4cd79f027cf016f6064927
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-12-19 12:12:40 +0100

    Rename class/file and update wrapper method names to match gym API more closely

--- mdm/utils/managed_env.py
+++ mdm/utils/managed_env.py
@@ -1,29 +1,37 @@
+from typing import Tuple
+
 import gym
+from gym.core import ObsType
 import numpy as np
 
 
-class EnvManager(gym.Wrapper):
+class ManagedEnv(gym.Wrapper):
 
     def __init__(self, env: gym.Env):
-        super(EnvManager, self).__init__(env)
+        super(ManagedEnv, self).__init__(env)
         self._curr_timestep = 0
 
     def is_first_step(self):
         return self._curr_timestep == 0
 
-    def interact(self, a):
+    def step(self, a):
         if self._curr_timestep == 0:  # by convention, make (a_0, r_0, t_0) = 0
             o = self.env.reset()
             a = np.zeros_like(self.env.action_space.sample())
             r = 0.0
             term = False
+            trunc = False
             info = {}
         else:
-            o, r, term, info = self.env.step(a)
-            if term:
+            o, r, term, trunc, info = self.env.step(a)
+            if term or trunc:
                 self._curr_timestep = 0
             else:
                 self._curr_timestep += 1
-        return o, a, r, term, info
+        return o, a, r, term, trunc, info
+
+    def reset(self, **kwargs) -> Tuple[ObsType, dict]:
+        self._curr_timestep = 0
+        return self.env.reset()
 
 
