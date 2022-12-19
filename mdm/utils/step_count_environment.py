2424423f117424944eb509526277b57207b2630c
Robin Schiewer <robin.schiewer@ini.rub.de>
2022-12-19 20:56:04 +0100

    Small wrapper that adds a step counte to a gym environment to track if a step() call will be the first in an episode or not

---
+++ mdm/utils/step_count_environment.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+
+import gym
+from gym.core import ObsType, ActType
+
+
+class StepCountEnv(gym.Wrapper):
+
+    def __init__(self,
+                 env: gym.Env):
+        super(StepCountEnv, self).__init__(env)
+        self.current_step = None
+
+    def reset(self,
+              **kwargs) -> Tuple[ObsType, dict]:
+        self.current_step = 0
+        return super(StepCountEnv, self).reset(**kwargs)
+
+    def step(self,
+             action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
+        o, r, term, trunc, info = self.env.step(action)
+        if term or trunc:
+            self.current_step = None
+        else:
+            self.current_step += 1
+        return o, r, term, trunc, info
