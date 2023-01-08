434521777cdb4de62c898797461bc3458e8dfe93
Robin Schiewer <robin.schiewer@ini.rub.de>
2023-01-08 16:17:44 +0100

    Add PredefinedPolicy class that takes a list of actions and provides one of them per call, intended for one-time use

---
+++ mdm/policies/predefined_policy.py
@@ -0,0 +1,20 @@
+import gym
+
+
+class PredefinedPolicy:
+
+    def __init__(self,
+                 env: gym.Env,
+                 action_list: gym.core.ActType):
+        self.env = env
+        self.action_list = action_list
+        self.current_timestep = 0
+        self.max_timestep = action_list.shape[0]
+
+    def __call__(self, *args):
+        if self.current_timestep < self.max_timestep:
+            a = self.action_list[self.current_timestep]
+            self.current_timestep += 1
+            return a
+        else:
+            raise StopIteration(f'No more actions available')
