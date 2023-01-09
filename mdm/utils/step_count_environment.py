a6729610a9641e9e8bc45067ff992db759352c0b
Robin Schiewer <robin.schiewer@ini.rub.de>
2023-01-09 12:28:19 +0100

    Add new wrappers to simplify and standardize data generation from gym environments

--- mdm/utils/step_count_environment.py
+++ mdm/utils/step_count_environment.py
@@ -1,7 +1,10 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union, List
 
 import gym
+import numpy as np
 from gym.core import ObsType, ActType
+
+from mdm.utils.utils import expand_shape_right
 
 
 class StepCountEnv(gym.Wrapper):
@@ -23,3 +26,95 @@ class StepCountEnv(gym.Wrapper):
         else:
             self.current_step += 1
         return o, r, term, trunc, info
+
+
+class CacheLastStepEnv(StepCountEnv):
+
+    def __init__(self,
+                 env: gym.Env):
+        super(CacheLastStepEnv, self).__init__(env)
+        self.last_o = None
+        self.last_a = None
+        self.last_r = None
+        self.last_term = None
+        self.last_trunc = None
+        self.last_info = None
+
+    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None) -> Tuple[ObsType, dict]:
+        o, info = self.env.reset(seed=seed, options=options)
+        self.last_o = o
+        self.last_a = np.zeros_like(self.action_space.sample())
+        self.last_r = 0.0
+        self.last_term = False
+        self.last_trunc = False
+        self.last_info = info
+        return o, info
+
+    def step(self,
+             action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
+        o, r, term, trunc, info = self.env.step(action)
+        self.last_o = o
+        self.last_a = action
+        self.last_r = r
+        self.last_term = term
+        self.last_trunc = trunc
+        self.last_info = info
+        return o, r, term, trunc, info
+
+
+class CacheLastStepVecEnv(gym.Wrapper):
+
+    def __init__(self,
+                 env: gym.vector.VectorEnv):
+        super(CacheLastStepVecEnv, self).__init__(env)
+        self.envs_done = np.full((self.env.num_envs,), False)
+        self.last_o = None
+        self.last_a = None
+        self.last_r = None
+        self.last_term = None
+        self.last_trunc = None
+        self.last_info = None
+        self.current_step = 0
+
+    @property
+    def all_envs_done(self):
+        return self.envs_done.all()
+
+    def reset(self,
+              *,
+              seed: Optional[Union[int, List[int]]] = None,
+              options: Optional[dict] = None):
+        self.envs_done[:] = False
+        o, infos = self.env.reset(seed=seed, options=options)
+        self.last_o = o
+        self.last_a = np.zeros_like(self.env.action_space.sample())
+        self.last_r = np.full((self.env.num_envs,), 0.0)
+        self.last_term = np.full((self.env.num_envs,), False)
+        self.last_trunc = np.full((self.env.num_envs,), False)
+        self.last_info = infos
+        self.current_step = 0
+        return o, infos
+
+    def step(self,
+             actions):
+        o, r, term, trunc, infos = self.env.step(actions)
+        self.last_o = np.where(expand_shape_right(self.envs_done, o), np.zeros_like(o), o)
+        self.last_a = np.where(expand_shape_right(self.envs_done, actions), np.zeros_like(actions), actions)
+        self.last_r = np.where(self.envs_done, np.zeros_like(r), r)
+        self.last_term = np.where(self.envs_done, np.zeros_like(term), term)
+        self.last_trunc = np.where(self.envs_done, np.zeros_like(trunc), trunc)
+        self.last_info = infos
+
+        done_now = np.bitwise_or(term, trunc)
+        if done_now.any():
+            mask = np.bitwise_and(infos['_final_observation'], self.envs_done)
+            mask = expand_shape_right(mask, o)
+            self.last_o = np.where(~mask, expand_shape_right(infos['final_observation'], o), o)  # TODO: check this
+        self.envs_done = np.bitwise_or(self.envs_done, done_now)
+
+        if self.envs_done.all():
+            self.current_step = 0
+        else:
+            self.current_step += 1
+
+        return o, r, term, trunc, infos
