8bbac5a3008c2b6bacda1a11b53c9bebdeedf04f
Robin Schiewer <robin.schiewer@ini.rub.de>
2023-02-09 14:21:57 +0100

    Add random walk success rate check

--- experiments/continuous_navigation/check_offline_data.py
+++ experiments/continuous_navigation/check_offline_data.py
@@ -6,15 +6,22 @@ import gym
 import gym_nav2d
 
 from mdm.memory.trajectory_memory import TrajectoryMemory
-from mdm.utils.utils import here, load_yaml, load_memory
+from mdm.utils.utils import here, load_yaml, load_memory, random_walk_success_rate
 from mdm.utils.analysis_tools import plot_trajectory_stats
 
 
 if __name__ == '__main__':
     cfg = load_yaml(here() / 'cfg_simple_rssm_train.yaml')
+    planning_cfg = load_yaml(here() / 'cfg_rssm_plan.yaml')
 
     map_version = 'VeryEasy'
     env = gym.make(f'gym_nav2d:nav2d{map_version}-v0')
+
+    success, r_ep, l_ep = random_walk_success_rate(env, 50, planning_cfg['n_rollouts'])
+    print(f'Random walk statistics with planning parameters:')
+    print(f'Expected initial success rate: {success}')
+    print(f'Expected initial average return: {r_ep.mean()}')
+    print(f'Expected initial average episode length: {l_ep.mean()}')
 
     train_mem = load_memory(here() / cfg['train_samples'])
     test_mem = load_memory(here() / cfg['test_samples'])
