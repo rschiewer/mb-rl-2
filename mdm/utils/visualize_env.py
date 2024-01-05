64e73f2c67e214f1490501a9b0083614e11c3c0e
Robin Schiewer <robin.schiewer@ini.rub.de>
2024-01-05 20:23:02 +0100

    Update plotscripts

--- mdm/utils/visualize_env.py
+++ mdm/utils/visualize_env.py
@@ -3,7 +3,7 @@ import gymnasium as gym
 from gym_nav2d.envs import Nav2dEnv
 from gymnasium_robotics.envs.maze.maze_v4 import MazeEnv
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, Ellipse
 
 from mdm.utils.utils import env_class_is, get_env_instance
 
@@ -19,6 +19,10 @@ def visualize_env(env: gym.Env, canvas: plt.Figure | plt.Axes, **kwargs):
     elif env_class_is(env, Nav2dEnv):
         plot_nav2d_env(env=env, canvas=canvas, **kwargs)
 
+#def draw_timestep(canvas: plt.Axes, x: float, y:float, i_t: int, size: float = 1, fontweight: int = 300):
+#    canvas.add_patch(Ellipse((x, y), width=0.05 * size, height=0.05 * size, fill=None))
+#    canvas.text(x, y, f'{t + 1}', c=c,
+#                horizontalalignment='center', verticalalignment='center_baseline', fontweight=fontweight)
 
 def plot_maze_env(env: gym.Env,
                   canvas: plt.Axes,
@@ -62,6 +66,9 @@ def plot_maze_env(env: gym.Env,
         for i_traj in range(observations.shape[1]):
             c = colors[i_traj % len(colors)]
             for t in range(observations.shape[0]):
+                if t == 0:
+                    canvas.scatter(observations[t, i_traj, 4], observations[t, i_traj, 5], marker=r'$\bigcirc$', c=c,
+                                   s=90)
                 canvas.scatter(observations[t, i_traj, 4], observations[t, i_traj, 5], marker=f'${t + 1}$', c=c, s=25)
     if goal_observations is not None:
         goal_observations = goal_observations[:, :n_trajs]
@@ -95,17 +102,24 @@ def plot_nav2d_env(env: gym.Env,
             c = colors[i_traj % len(colors)]
             # plot individual time steps
             for t in range(observations.shape[0]):
-                canvas.scatter(observations[t, i_traj, 0], observations[t, i_traj, 1], marker=f'${t + 1}$', c=c, s=25)
+                fontweight = 900 if t == 0 else 300
+                x, y = observations[t, i_traj, :2]
+                border = Ellipse((x, y), width=0.05, height=0.05, fill=True, color='white', zorder=i_traj+t*0.01)
+                canvas.add_patch(border)
+                border = Ellipse((x, y), width=0.05, height=0.05, fill=False, color=c, zorder=i_traj+(t+0.25)*0.01)
+                canvas.add_patch(border)
+                canvas.text(x, y, f'{t + 1}', c=c, horizontalalignment='center', verticalalignment='center_baseline',
+                            fontweight=fontweight, fontsize='small', zorder=i_traj+(t+0.5)*0.01)
             # plot reward location for completeness
             reward_location = observations[0, i_traj, 2:4]
-            canvas.scatter(reward_location[0], reward_location[1], marker='.', c=c, s=500)
-            canvas.scatter(reward_location[0], reward_location[1], marker='$R$', c='white', s=45)
+            canvas.scatter(reward_location[0], reward_location[1], marker='x', c=c, s=50, zorder=i_traj+(t+0.75)*0.01)
     if goal_observations is not None:
         goal_observations = goal_observations[:, :n_trajs]
         for i_traj in range(goal_observations.shape[1]):
             c = colors[i_traj % len(colors)]
             for t in range(goal_observations.shape[0]):
-                canvas.scatter(goal_observations[t, i_traj, 0], goal_observations[t, i_traj, 1],
-                               marker='h', c=c, s=80)
-                canvas.scatter(goal_observations[t, i_traj, 0], goal_observations[t, i_traj, 1],
-                               marker=f'${t + 1}$', c='white', s=25)
+                x, y = goal_observations[t, i_traj, :2]
+                canvas.scatter(x, y, marker='h', c=c, s=200, zorder=n_trajs+t*0.01)
+                canvas.text(x, y, f'{t + 1}', c='white', horizontalalignment='center',
+                            verticalalignment='center_baseline', fontweight=900, fontsize='medium',
+                            zorder=n_trajs+(t+0.5)*0.01)
