c576bc24f96412ebc8b9c9ff4961be27bcee84d0
Robin Schiewer <robin.schiewer@ini.rub.de>
2024-01-05 17:31:24 +0100

    Use new implicit conversion routines for log_plot() and visualize_env() in goal seeking agent train routine

--- mdm/training/train.py
+++ mdm/training/train.py
@@ -647,10 +647,8 @@ def train_gsa(model, level, pred, targets, eval_env, cfg, i_step, logger, sample
         obs = numpyfy(goal_simulation['model']['o'])
         goal_obs = numpyfy(goal_obs)[None]  # need to add a time dimension to goal observations as well
         with TempFigure(figsize=(10, 10)) as fig:
-            visualize_env(eval_env, fig.gca(), observations=obs, goal_observations=goal_obs, n_trajs=2)
-            plt.show()
-            logger.log_plot(fig_to_img(fig),
-                            Scope.TRAIN() / f'goal_seeking_agent/{level}/goal_seeking_train_performance', i_step)
+            visualize_env(eval_env, fig, observations=obs, goal_observations=goal_obs, n_trajs=2)
+            logger.log_plot(fig, Scope.TRAIN() / f'goal_seeking_agent/{level}/goal_seeking_train_performance', i_step)
 
 
 def train_goal_seeking_agent_goals_above_with_agent(model, level, pred, targets, eval_env, cfg, i_step, logger,
