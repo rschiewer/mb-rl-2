0b6c92af9f4938db7fad67a6ed2e59b0fd228d6f
Robin Schiewer <robin.schiewer@ini.rub.de>
2024-01-13 16:55:12 +0100

    Add new version of PointMaze with C-shaped maze and maximum distance between start and reward

--- mdm/utils/customized_gym_envs.py
+++ mdm/utils/customized_gym_envs.py
@@ -6,9 +6,22 @@ static_u_maze_map_small = [[1, 1, 1, 1, 1],
                            [1, 'r', 0, 0, 1],
                            [1, 1, 1, 1, 1]]
 
+static_u_maze_map_small_far = [[1, 1, 1, 1, 1],
+                               [1, 'g', 0, 0, 1],
+                               [1, 1, 1, 0, 1],
+                               [1, 'r', 0, 0, 1],
+                               [1, 1, 1, 1, 1]]
+
 gym.register(
     id=f"PointMaze_UMaze_static-v3",
     entry_point="gymnasium_robotics.envs.maze.point_maze:PointMazeEnv",
     kwargs={"maze_map": static_u_maze_map_small, 'continuing_task': False, 'reset_target': False},
+    max_episode_steps=300,
+)
+
+gym.register(
+    id=f"PointMaze_UMaze_static_far-v3",
+    entry_point="gymnasium_robotics.envs.maze.point_maze:PointMazeEnv",
+    kwargs={"maze_map": static_u_maze_map_small_far, 'continuing_task': False, 'reset_target': False},
     max_episode_steps=300,
 )
